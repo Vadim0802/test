2a66d9fa21f53a486250ae94f0e2257466375489
VadimB <v.bodyansky@yandex.ru>
2020-08-23 15:20:06 +1000

    refactor prettier

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash'
+import _ from "lodash";
 
 const factorial = (n) => {
-	if (n === 0) {
-		return 1;
-	}
-	return n * factorial(n - 1);
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 console.log(factorial(5));
